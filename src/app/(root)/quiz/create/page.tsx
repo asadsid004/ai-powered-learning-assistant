450bc8e015450b6131ead39f6315ca394d1c5317
asadsid004 <workspaceasad2@gmail.com>
2025-03-18 21:09:46 +0530

    feat: Created flashcard form and added server action to store it in database

--- src/app/(root)/quiz/create/page.tsx
+++ src/app/(root)/quiz/create/page.tsx
@@ -7,37 +7,38 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Tabs, TabsList } from "@/components/ui/tabs";
-import { TabsContent, TabsTrigger } from "@radix-ui/react-tabs";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
 
 const CreateQuizPage = () => {
   return (
-    <div>
-      <Card className="m-4 border-none shadow-none max-w-3xl mx-auto">
-        <CardHeader>
-          <CardTitle className="text-2xl tracking-tight font-bold">
-            Create Quiz
-          </CardTitle>
-          <CardDescription className="tracking-wide">
-            Create personalized quizzes to check your knowledge
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="w-full">
-          <Tabs className="w-full">
-            <TabsList className="grid w-full selection:bg-emerald-50 grid-cols-2">
-              <TabsTrigger value="topic">Topic</TabsTrigger>
-              <TabsTrigger value="upload">Upload</TabsTrigger>
-            </TabsList>
-            <TabsContent value="topic">
-              <CreateQuizForm />
-            </TabsContent>
-            <TabsContent value="upload">
-              <CreateuizFormWithPdf />
-            </TabsContent>
-          </Tabs>
-        </CardContent>
-      </Card>
-    </div>
+    <Card className="m-4 border-none shadow-none max-w-3xl mx-auto">
+      <CardHeader>
+        <CardTitle className="text-2xl tracking-tight font-bold">
+          Create Quiz
+        </CardTitle>
+        <CardDescription className="tracking-wide">
+          Create personalized quizzes to check your knowledge
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="w-full">
+        <Tabs className="w-full" defaultValue="topic">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger className="cursor-pointer" value="topic">
+              Topic
+            </TabsTrigger>
+            <TabsTrigger className="cursor-pointer" value="upload">
+              Upload
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="topic">
+            <CreateQuizForm />
+          </TabsContent>
+          <TabsContent value="upload">
+            <CreateuizFormWithPdf />
+          </TabsContent>
+        </Tabs>
+      </CardContent>
+    </Card>
   );
 };
 
